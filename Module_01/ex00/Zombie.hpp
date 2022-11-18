af6d29a77d55fc932c49c4142e567743b89fc20a
Delphine Bouron <dbouron@student.42lyon.fr>
2022-11-18 14:39:09 +0100

    Module_01 - Ex00: created files

---
+++ Module_01/ex00/Zombie.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 14:33:37 by dbouron           #+#    #+#             */
+/*   Updated: 2022/11/18 14:33:37 by dbouron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PISCINE_CPP_ZOMBIE_HPP
+#define PISCINE_CPP_ZOMBIE_HPP
+
+
+class Zombie
+{
+
+};
+
+
+#endif //PISCINE_CPP_ZOMBIE_HPP
