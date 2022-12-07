fc3bbda7e0581183963bab1c677ee0e24fc48e4b
Delphine Bouron <dbouron@student.42lyon.fr>
2022-12-07 18:56:37 +0100

    Module 02 - ex03: change output presentation

--- Module_02/ex03/main.cpp
+++ Module_02/ex03/main.cpp
@@ -21,7 +21,7 @@ int	main()
 	Point const	c1(210.6f, 0);
 	Point const	point1(111.6, 63.4);
 
-	std::cout << "Triangle 1 (point is inside the triangle, result should be \"inside\") :					";
+	std::cout << "Triangle 1 (point is inside the triangle, result should be \"inside\") : ";
 	if (bsp(a1, b1, c1, point1)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a2(0, 0);
@@ -29,7 +29,7 @@ int	main()
 	Point	c2(20.6f, 1.2f);
 	Point	point2(10.6f, 45.2f);
 
-	std::cout << "Triangle 2 (point is outside the triangle, result should be \"outside\") :				";
+	std::cout << "Triangle 2 (point is outside the triangle, result should be \"outside\") : ";
 	if (bsp(a2, b2, c2, point2)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a3(0, 0);
@@ -37,7 +37,7 @@ int	main()
 	Point	c3(20.6f, 0);
 	Point	point3(10.6f, 0);
 
-	std::cout << "Triangle 3 (point is on the line AC of the triangle, result should be \"outside\") :			";
+	std::cout << "Triangle 3 (point is on the line AC of the triangle, result should be \"outside\") : ";
 	if (bsp(a3, b3, c3, point3)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a4(2, 2);
@@ -45,7 +45,7 @@ int	main()
 	Point	c4(8, 42);
 	Point	point4(8, 12);
 
-	std::cout << "Triangle 4 (point is on the line BC of the triangle, result should be \"outside\") :			";
+	std::cout << "Triangle 4 (point is on the line BC of the triangle, result should be \"outside\") : ";
 	if (bsp(a4, b4, c4, point4)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a5(2, 2);
@@ -53,7 +53,7 @@ int	main()
 	Point	c5(6, 1);
 	Point	point5(4, 4);
 
-	std::cout << "Triangle 5 (point is on the line AB of the triangle, result should be \"outside\") :			";
+	std::cout << "Triangle 5 (point is on the line AB of the triangle, result should be \"outside\") : ";
 	if (bsp(a5, b5, c5, point5)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a6(12.669551f, 24.265f);
@@ -61,7 +61,7 @@ int	main()
 	Point	c6(18.0236f, -6.964f);
 	Point	point6(10.15f, 3.4f);
 
-	std::cout << "Triangle 6 (point is inside the triangle with negative values, result should be \"inside\") :		";
+	std::cout << "Triangle 6 (point is inside the triangle with negative values, result should be \"inside\") : ";
 	if (bsp(a6, b6, c6, point6)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	Point	a7(12.669551f, 24.265f);
@@ -69,7 +69,7 @@ int	main()
 	Point	c7(18.0236f, -6.964f);
 	Point	point7(-2.3f, (-2.3f * 0.316406f + 20.2578f));
 
-	std::cout << "Triangle 7 (point is on the line AB of the triangle with negative values, result should be \"outside\") :	";
+	std::cout << "Triangle 7 (point is on the line AB of the triangle with negative values, result should be \"outside\") : ";
 	if (bsp(a7, b7, c7, point7)) { std::cout << "INSIDE\n"; } else { std::cout << "OUTSIDE\n"; }
 
 	return (0);
