44d3a4352f51f2904596ca235e13654519fb2e8e
Delphine Bouron <dbouron@student.42lyon.fr>
2022-12-30 20:56:15 +0100

    Module 05 - ex01: add tests in main

--- Module_05/ex01/main.cpp
+++ Module_05/ex01/main.cpp
@@ -16,21 +16,34 @@ int main()
 {
 	try
 	{
-		Bureaucrat Albert("Albert", 80);
+		Bureaucrat Albert("Albert", 50);
 		Bureaucrat Alice("Alice", 1);
 		//Bureaucrat Albane("Albane", 500);
 		Bureaucrat Random;
+
+		Form A;
+		Form B("B", 30, 50);
 
 		std::cout << "\n ─── BUREAUCRATS ───" << std::endl;
 		std::cout << Albert << std::endl;
 		std::cout << Alice << std::endl;
 		std::cout << Random << std::endl;
 
+		std::cout << "\n ─── FORMS ───" << std::endl;
+		std::cout << A << std::endl;
+		std::cout << B << std::endl;
+
 		std::cout << "\n ─── ALBERT ───" << std::endl;
+		A.beSigned(Albert);
+		Albert.signForm(A);
+		B.beSigned(Albert);
+		Albert.signForm(B);
 		Albert.decrementGrade();
 		std::cout << Albert << std::endl;
-		Albert.incrementGrade();
-		std::cout << Albert << std::endl;
+		A.beSigned(Albert);
+		Albert.signForm(A);
+		B.beSigned(Albert);
+		Albert.signForm(B);
 
 		std::cout << "\n ─── ALICE ───" << std::endl;
 		Alice.incrementGrade();
