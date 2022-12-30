b4e0c75bb5d1edd4fc2909f39394cd051f66114a
Delphine Bouron <dbouron@student.42lyon.fr>
2022-12-30 20:31:44 +0100

    Module 05 - ex01: add Bureaucrat class

---
+++ Module_05/ex01/Bureaucrat.hpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 12:07:38 by dbouron           #+#    #+#             */
+/*   Updated: 2022/12/29 12:07:38 by dbouron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <stdexcept>
+# include "Form.hpp"
+
+# define MAX_GRADE 1
+# define MIN_GRADE 150
+
+class Form;
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat &src);
+		~Bureaucrat();
+
+		Bureaucrat &operator=(const Bureaucrat &rhs);
+
+		const std::string &getName() const;
+		int getGrade() const;
+		void incrementGrade();
+		void decrementGrade();
+
+		void signForm(Form &form);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Bureaucrat: grade is too high");
+				}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Bureaucrat: grade is too low");
+				}
+		};
+
+	private:
+		std::string	_name;
+		int 		_grade;
+};
+
+std::ostream &	operator<<(std::ostream & o, Bureaucrat const & rhs);
+
+#endif //BUREAUCRAT_HPP
