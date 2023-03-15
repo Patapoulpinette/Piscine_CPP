1fd8d635912c1a6cb3f94fbcc5dccdb34fad827b
dbouron <dbouron@student.42lyon.fr>
2023-03-15 11:58:54 +0100

    Add BitcoinExchange files class files

---
+++ Module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 11:45:59 by dbouron           #+#    #+#             */
+/*   Updated: 2023/03/15 11:45:59 by dbouron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+
+class BitcoinExchange
+{
+	public:
+
+	private:
+
+};
+
+#endif //BITCOINEXCHANGE_HPP
