41d590e474f4f122a1ef12f88396d6238b0c1f20
dbouron <dbouron@student.42lyon.fr>
2023-03-31 17:34:26 +0200

    Move a line in main

--- Module_09/ex02/main.cpp
+++ Module_09/ex02/main.cpp
@@ -32,23 +32,23 @@ int main(int argc, char **argv)
 		std::cout << std::endl;
 
 		//Sorting with Vector --------------------------------------------------
-		vectorTime = clock();
 		std::vector<unsigned int> uIntVector = convert<std::vector<unsigned int>, std::vector<std::string> >(strVector);
 		PmergeMe<std::vector<unsigned int> > vector(uIntVector);
+		vectorTime = clock();
 		vector.sort(vector.getList(), 0, static_cast<int>(vector.getList().size() - 1));
 		vectorTime = clock() - vectorTime;
 
 		//Sorting with Deque ---------------------------------------------------
-		dequeTime = clock();
 		std::deque<unsigned int> uIntDeque = convert<std::deque<unsigned int>, std::deque<std::string> >(strDeque);
 		PmergeMe<std::deque<unsigned int> > deque(uIntDeque);
+		dequeTime = clock();
 		deque.sort(deque.getList(), 0, static_cast<int>(deque.getList().size() - 1));
 		dequeTime = clock() - dequeTime;
 
 		//Display outputs ------------------------------------------------------
 		vector.print("Sorted with vector");
+		deque.print("Sorted with Deque"); //TODO to remove (test)
 		std::cout << "Time to sort with vector: " << static_cast<float>(vectorTime) / CLOCKS_PER_SEC * 1000 << " ms" << std::endl;
-		//deque.print("Sorted with Deque"); //TODO to remove (test)
 		std::cout << "Time to sort with deque: " << static_cast<float>(dequeTime) / CLOCKS_PER_SEC * 1000 << " ms" << std::endl;
 	}
 	return 0;
